5cb74e1995efa28fea1efabac8d6a566a7b8dc5f
Patricia Uribe <uribezuletapatricia@gmail.com>
2022-09-20 18:14:56 -0500

    Fibonacci supervisor

---
+++ lib/AppSophos/fibonacciServer.ex
@@ -0,0 +1,36 @@
+defmodule SophosApp.FibonacciServer do
+  alias SophosApp.Fibonacci
+
+  def start() do
+    spawn(__MODULE__, :loop, [])
+  end
+
+  def start_link() do
+    spawn_link(__MODULE__, :loop, [])
+  end
+
+  def start_monitor() do
+    spawn_monitor(__MODULE__, :loop, [])
+  end
+
+  def loop() do
+    receive do
+      {:sequence, caller, n} ->
+        result = Fibonacci.sequence(n)
+        send(caller, {:fibonacci, n, result})
+        loop()
+
+      {:status, caller, msg} ->
+        send(caller, {:status, :ok, msg})
+        loop()
+
+      {:exit, reason} ->
+        IO.puts("bye for #{inspect(reason)}")
+
+      _message ->
+        IO.puts("Bad operation")
+        loop()
+
+    end
+  end
+end
